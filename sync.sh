c9753e71918d7ebd345a11606a6f935ac53c0362
Aolin <aolin.zhang@pingcap.com>
2023-06-21 13:36:20 +0800

    fix tidb-cloud and operator docs preview

--- sync.sh
+++ sync.sh
@@ -4,48 +4,47 @@ set -e
 
 # Get the directory of this script
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 cd "$SCRIPT_DIR"
 
-# Ensure that jq is installed and GITHUB_TOKEN is set
-# jq is used to parse JSON response from GitHub API
-which jq &> /dev/null || (echo "jq is not installed. see https://stedolan.github.io/jq/download/" && exit 1)
-# GITHUB_TOKEN is used to access GitHub API (Get a pull request)
-test -n "$GITHUB_TOKEN" || (echo "GITHUB_TOKEN is not set, repo scope is needed" && exit 1)
+# Verify if jq is installed and GITHUB_TOKEN is set.
+which jq &> /dev/null || (echo "Error: jq is not installed, but is required for this script to parse JSON response from GitHub API. You can download and install jq from https://stedolan.github.io/jq/download/" && exit 1)
+test -n "$GITHUB_TOKEN" || (echo "Error: The GITHUB_TOKEN environment variable is not set. This token is required for accessing the GitHub API and needs to have the repo scope." && exit 1)
 
-# If branch name not provided as argument, use the current branch
+# If the branch name is not provided as an argument, use the current branch.
 BRANCH_NAME=${1:-$(git branch --show-current)}
 
-# Extract product, repo owner, repo name and PR number from branch name. The name pattern is r"preview(-cloud|-operator)?/pingcap/docs(-cn|-tidb-operator)?/[0-9]+)"
+# Extract product, repo owner, repo name, and PR number from the branch name. The name pattern is r"preview(-cloud|-operator)?/pingcap/docs(-cn|-tidb-operator)?/[0-9]+)"
 PREVIEW_PRODUCT=$(echo "$BRANCH_NAME" | cut -d'/' -f1)
 REPO_OWNER=$(echo "$BRANCH_NAME" | cut -d'/' -f2)
 REPO_NAME=$(echo "$BRANCH_NAME" | cut -d'/' -f3)
 PR_NUMBER=$(echo "$BRANCH_NAME" | cut -d'/' -f4)
 
+# Get the base branch of this PR <https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#get-a-pull-request>
+BASE_BRANCH=$(curl -fsSL -H "Authorization: token $GITHUB_TOKEN" \
+    "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/pulls/$PR_NUMBER" | \
+    jq -r '.base.ref')
+
+# Ensure that BASE_BRANCH is not empty
+test -n "$BASE_BRANCH" || (echo "Error: Cannot get BASE_BRANCH." && exit 1)
+
 # Determine product name based on the PREVIEW_PRODUCT
 case "$PREVIEW_PRODUCT" in
   preview)
-    PRODUCT_NAME="tidb"
+    DIR_SUFFIX="tidb/${BASE_BRANCH}"
     ;;
   preview-cloud)
-    PRODUCT_NAME="tidbcloud"
+    DIR_SUFFIX="tidbcloud/master"
     ;;
   preview-operator)
-    PRODUCT_NAME="tidb-in-kubernetes"
+    DIR_SUFFIX="tidb-in-kubernetes/${BASE_BRANCH}"
     ;;
   *)
-    echo "Error: Branch name must start with preview/, preview-cloud/, or preview-operator/"
+    echo "Error: Branch name must start with preview/, preview-cloud/, or preview-operator/."
     exit 1
     ;;
 esac
 
-# TODO: remove this check
-# Ensure repo owner is pingcap
-test "$REPO_OWNER" == "pingcap" || (echo "Error: The repo owner can only be pingcap" && exit 1)
-
 # Define sync tasks for different repos
-# TODO: confrim the command
-declare -a SYNC_TASKS
 case "$REPO_NAME" in
   docs)
     # sync all modified or added files from root dir to markdown-pages/en/
@@ -61,50 +60,40 @@ case "$REPO_NAME" in
     SYNC_TASKS=("en/,en/" "zh/,zh/")
     ;;
   *)
-    echo "Error: Invalid repo name"
+    echo "Error: Invalid repo name. Only docs, docs-cn, and docs-tidb-operator are supported."
     exit 1
     ;;
 esac
 
 REPO_DIR="src/$REPO_NAME"
 
-# TODO: if REPO_DIR exists, need to fetch PREVIEW_BRANCH
 # Clone repo if it doesn't exist already
 test -e "$REPO_DIR/.git" || git clone "https://github.com/$REPO_OWNER/$REPO_NAME.git" "$REPO_DIR"
 
 # --update-head-ok: By default git fetch refuses to update the head which corresponds to the current branch. This flag disables the check. This is purely for the internal use for git pull to communicate with git fetch, and unless you are implementing your own Porcelain you are not supposed to use it.
 # use --force to overwrite local branch when remote branch is force pushed
+git -C "$REPO_DIR" fetch origin "$BASE_BRANCH" #<https://stackoverflow.com/questions/33152725/git-diff-gives-ambigious-argument-error>
 git -C "$REPO_DIR" fetch origin pull/"$PR_NUMBER"/head:PR-"$PR_NUMBER" --update-head-ok --force
 git -C "$REPO_DIR" checkout PR-"$PR_NUMBER"
-
-# Get the base branch of this PR <https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#get-a-pull-request>
-PREVIEW_BRANCH=$(curl -fsSL -H "Authorization: token $GITHUB_TOKEN" \
-    "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/pulls/$PR_NUMBER" | \
-    jq -r '.base.ref')
-
-# Ensure that PREVIEW_BRANCH is not empty
-
-test -n "$PREVIEW_BRANCH" || (echo "cannot get PREVIEW_BRANCH" && exit 1)
-# git -C "$REPO_DIR" fetch origin "$PREVIEW_BRANCH" <https://stackoverflow.com/questions/33152725/git-diff-gives-ambigious-argument-error>
 
 # Perform sync tasks
 for TASK in "${SYNC_TASKS[@]}"; do
 
   SRC_DIR=$(echo "$TASK" | cut -d',' -f1)
-  DEST_DIR="markdown-pages/$(echo "$TASK" | cut -d',' -f2)/$PRODUCT_NAME/$PREVIEW_BRANCH"
-  if [ "$PRODUCT_NAME" == "tidbcloud" ]; then
-    DEST_DIR="markdown-pages/$(echo "$TASK" | cut -d',' -f2)/$PRODUCT_NAME/master"
-  fi
+  DEST_DIR="markdown-pages/$(echo "$TASK" | cut -d',' -f2)/$DIR_SUFFIX"
   mkdir -p "$DEST_DIR"
   # Only sync modified or added files
-  git -C "$REPO_DIR" diff --merge-base --name-only --diff-filter=AM origin/"$PREVIEW_BRANCH" "$SRC_DIR" | tee /dev/fd/2 | \
-    rsync -av --files-from=- "$REPO_DIR/$SRC_DIR" "$DEST_DIR"
+  git -C "$REPO_DIR" diff --merge-base --name-only --diff-filter=AM origin/"$BASE_BRANCH" "$SRC_DIR" | tee /dev/fd/2 | \
+    rsync -av --files-from=- "$REPO_DIR" "$DEST_DIR"
 
 done
 
+test -n "$TEST" && echo "Test mode, exiting..." && exit 0
+
 # Get the current commit SHA
 CURRENT_COMMIT=$(git -C "$REPO_DIR" rev-parse HEAD)
+
 # Handle untracked files
 git add .
 # Commit changes, if any
-git commit -m "Preview PR https://github.com/$REPO_OWNER/$REPO_NAME/pull/$PR_NUMBER and this preview is trigged from commit https://github.com/$REPO_OWNER/$REPO_NAME/pull/$PR_NUMBER/commits/$CURRENT_COMMIT" || echo "No changes to commit"
+git commit -m "Preview PR https://github.com/$REPO_OWNER/$REPO_NAME/pull/$PR_NUMBER and this preview is triggered from commit https://github.com/$REPO_OWNER/$REPO_NAME/pull/$PR_NUMBER/commits/$CURRENT_COMMIT" || echo "No changes to commit"
